43395896de8a9eb3f04ed57d924c3860d65aad45
Vadim <wolfindeveloper@gmail.com>
2018-08-20 17:21:47 +0300

    freelancehunt nalogdelete

--- nalogovoia/js/cryptography/drivers/cryptopro.extension.js?v=18085.js
+++
@@ -1,8 +0,0 @@
-
-CryptoApi.prototype.getCspName = function() {
-    return this.pluginInfo.cspName;
-};
-
-CryptoApi.prototype.getCspVersion = function(e) {
-    return this.versionToString(this.pluginInfo.cspVersion);
-};
